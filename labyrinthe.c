51c41a5ab1ad9ada1c0044a758a73e1ea78fdfe6
youkwhd <lolywk@tutanota.com>
2024-02-24 17:10:06 +0700

    feat: initial main source

---
+++ labyrinthe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "labyrinthe.h"
+
+int main(int argc, char **argv)
+{
+    UNUSED(argc);
+    UNUSED(argv);
+
+    printf("Hello from Labyrinthe!\n");
+    return 0;
+}
---
+++ labyrinthe.h
@@ -0,0 +1,6 @@
+#ifndef __LABYRINTHE_H
+#define __LABYRINTHE_H
+
+#define UNUSED(var) (void)var
+
+#endif // __LABYRINTHE_H
