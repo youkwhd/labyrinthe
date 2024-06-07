bc79b81aa97b6846ccecfac36b7d72cd28972e73
youkwhd <lolywk@tutanota.com>
2024-06-07 22:05:44 +0700

    feat: add queue

---
+++ queue.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+#include "queue.h"
+
+void queue_init(queue_t *q, size_t size)
+{
+    q->cursor = 0;
+    q->size = size;
+    q->arr = malloc((sizeof *q->arr) * q->size);
+}
+
+bool queue_is_empty(queue_t *q)
+{
+    return q->cursor == 0;
+}
+
+void queue_enqueue(queue_t *q, coordinate_t x)
+{
+    q->arr[q->cursor++] = x;
+}
+
+coordinate_t queue_head(queue_t *q)
+{
+    return q->arr[0];
+}
+
+coordinate_t queue_dequeue(queue_t *q)
+{
+    coordinate_t val = q->arr[0];
+
+    for (int i = 1; i < q->cursor; i++)
+        q->arr[i - 1] = q->arr[i];
+
+    return val;
+}
---
+++ queue.h
@@ -0,0 +1,20 @@
+#ifndef __LABYRINTHE_QUEUE_H
+#define __LABYRINTHE_QUEUE_H
+
+#include <stdbool.h>
+#include <stddef.h>
+
+#include "coordinate.h"
+
+typedef struct queue_t {
+    size_t cursor, size;
+    coordinate_t *arr;
+} queue_t;
+
+void queue_init(queue_t *q, size_t size);
+bool queue_is_empty(queue_t *q);
+void queue_enqueue(queue_t *q, coordinate_t x);
+coordinate_t queue_head(queue_t *q);
+coordinate_t queue_dequeue(queue_t *q);
+
+#endif // __LABYRINTHE_QUEUE_H
