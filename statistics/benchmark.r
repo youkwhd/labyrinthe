6c36ff07ad21918cf8fce3a584d283655a5eb72e
youkwhd <lolywk@tutanota.com>
2024-06-09 19:27:18 +0700

    chore: add shit benchmark

--- statistics/benchmark.r
+++ statistics/benchmark.r
@@ -1,3 +1,27 @@
 #!/usr/bin/Rscript
 
-print("Hello from R!")
+library(gridExtra)
+library(grid)
+library(purrr)
+
+labyrinthe.strategies <- c("best", "breadth", "depth")
+labyrinthe.size <- 30
+
+timeElapsed <- function (f) {
+    start <- Sys.time()
+    f()
+    end <- Sys.time()
+    as.numeric(end - start)
+}
+
+for (strat in labyrinthe.strategies) {
+    print(strat)
+
+    for (i in 1:10) {
+        cmd <- paste0("./labyrinthe -S", " --strat=", strat, " --width=", labyrinthe.size * i, " --height=", labyrinthe.size * i)
+        elapsed <- timeElapsed(function () system(cmd, ignore.stdout=TRUE))
+        print(labyrinthe.size * i)
+        print(elapsed)
+        print("", quote=FALSE)
+    }
+}
